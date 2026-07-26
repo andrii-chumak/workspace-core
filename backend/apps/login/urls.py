f80408ba127ddb5a3392222f3aa9657062537530
Денис Заставний <denys.zastavnyi@lpnu.ua>
2026-07-26 21:35:54 +0300

    fix

--- backend/apps/login/urls.py
+++ backend/apps/login/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from drf_spectacular.utils import extend_schema
-from apps.register.views import GoogleAuthView
 from .views import LoginAPIView
 
 PublicTokenRefreshView = extend_schema(auth=[])(TokenRefreshView)
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='api_login'),
-    path('login/google/', GoogleAuthView.as_view(), name='api_login_google'),
     path('token/refresh/', PublicTokenRefreshView.as_view(), name='api_token_refresh'),
 ]
