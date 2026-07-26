02688242bd5bc8f1265569905a4b3af0226ba749
andrii-chumak <andrii.chumak.dev@gmail.com>
2026-07-26 12:06:04 +0300

    feat(LoginModule): added google login

--- backend/apps/login/urls.py
+++ backend/apps/login/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
+
+from apps.register.views import GoogleAuthView
 from .views import LoginAPIView
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='api_login'),
+    path('login/google/', GoogleAuthView.as_view(), name='api_login_google'),
     path('token/refresh/', TokenRefreshView.as_view(), name='api_token_refresh'),
 ]
